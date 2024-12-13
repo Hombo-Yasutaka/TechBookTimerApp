70bf922e669ba21386791631844fd47dbdff186f
Hombo-Yasutaka <y.honbo@genbasupport.com>
2024-12-13 17:43:23 +0900

    Initial Commit

---
+++ TechBookTimerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TechBookTimerApp
+//
+//  Created by 本坊康孝 on 2024/12/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TechBookTimerAppTests/TechBookTimerAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TechBookTimerAppTests.swift
+//  TechBookTimerAppTests
+//
+//  Created by 本坊康孝 on 2024/12/13.
+//
+
+import Testing
+@testable import TechBookTimerApp
+
+struct TechBookTimerAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
