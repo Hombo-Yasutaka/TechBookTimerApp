91b42242d3ad502e88bc29e508f78e8ca918ddec
Hombo-Yasutaka <y.honbo@genbasupport.com>
2024-12-13 18:00:22 +0900

    事前準備

---
+++ TechBookTimerApp/HomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HomeViewController.swift
+//  TechBookTimerApp
+//
+//  Created by 本坊康孝 on 2024/12/13.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
